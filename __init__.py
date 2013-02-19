580b04601082dc5b3864867104f9c6aaba37e5d3
Christopher Durkin <cjdurkin@gmail.com>
2013-02-18 23:45:03 -0500

    Added more documentation and todo list

--- __init__.py
+++ __init__.py
@@ -1,3 +1,27 @@
+"""bigfont, a pure python library for rendering large fonts in a console
+using fixed-width console characters.
+
+Simple usage:
+
+  import bigfont
+  bigfont.bigprint("hello")
+
+This loads the first default font from those included in the fonts
+directory of this library, and prints it.
+
+To load a specific font from an flf file:
+
+  from font import font_from_file
+  myfont = font_from_file('path_to_myfont.flf')
+  myfont.bigprint("hello, myfont")
+  mystrvar = myfont.render("i will print this myself")
+  print(mystrvar)
+
+More fonts can be obtained from:
+http://www.jave.de/figlet/fonts.html
+"""
+
+
 #from letter import BigLetter
 from font import BigFont
 #from smoosh import Smoosher
