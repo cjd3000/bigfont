2f9fc3194ac282607f3ac60bde6f29825e3b2d74
Christopher Durkin <cjdurkin@gmail.com>
2013-04-22 22:32:16 -0400

    Use header info when loading fonts
    Parse the header info, still needs to be passed to smoosher. Also
    correctly load extended optional characters.

--- font.py
+++ font.py
@@ -11,11 +11,15 @@ import pickle
 import inspect
 import traceback
 import gzip
+import sys
 
 from zipfile import ZipFile, BadZipfile # or BadZipFile in 3.x
 from base import BaseObject
 from letter import BigLetter
 from decorators import trace
+
+class BigFontError(Exception):
+    pass
 
 def bigprint(text,font=None):
     """Print text in big font.
@@ -89,7 +93,12 @@ class BigFont(BaseObject):
     def __init__(self,data=None,name=None,nonprintable=32,eol="\n",**kwargs):
         super(BigFont,self).__init__(**kwargs)
         self.nonprintable = nonprintable
-        self.letters = self._extract_letters(data)
+        try:
+            self.letters = self._extract_letters(data)
+        except:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            logging.warn("load failed, %s: %s " % (exc_type,exc_value))
+            raise BigFontError("font data did not make sense")
         #self.renderfcn = kern
         self.eol = eol
         self.name = name
@@ -105,38 +114,79 @@ class BigFont(BaseObject):
         else:
             lines = [line.rstrip() for line in data]
             
-        header = lines[0]
-        logging.info("ignoring header (FIXME)")
-        endchar = '@'
-        logging.info("endchar is hardcoded to %s (FIXME)" % endchar)
+        header = self._parse_header(lines[0])
+        # last char in first data line is the endchar
+        endchar = lines[header['comment_lines'] + 1][-1]
         buf = []
-        letters = [[]]*self.nonprintable
+        letters = [ None for _ in xrange(256) ]
+        index = 32 # first required character index
+        optional_chars = re.compile(r"""^(\d+)\b.+(?<!%s)$""" % endchar)
+        
         for line in lines:
+            if index > 126: # required chars
+                m = re.match(optional_chars, line)
+                if m:
+                    index = int(m.group(1))
+                    continue
             if len(line) > 1 and line[-2:] == (endchar*2):
                 buf.append(line[:-2])
-                letters.append(BigLetter(buf))
+                print(index)
+                letters[index] = BigLetter(buf,hardblank=header['hardblank'])
+                print(letters[index])
+                index += 1
                 buf = []
             elif len(line) > 0 and line[-1] == endchar:
                 buf.append(line[:-1])
+
+        # copy required german characters to their correct positions
+        moveto = (196,214,220,228,246,252,223)
+        for idx,char in enumerate(moveto):
+            if letters[char] is None and letters[idx+127] is not None:
+                letters[char] = letters[idx+127]
+                letters[idx+127] = None
+
         return letters
 
+    def __iter__(self):
+        for letter in self.letters:
+            if letter is not None:
+                yield letter
+
     @trace
+    def _parse_header(self,hdr):
+        """Retrieve info from FIGfont header line, return as dict."""
+        out = {}
+        if len(hdr) < 5 or hdr[0:5] != "flf2a":
+            raise BigFontError("did not understand header %s" % hdr)
+        fields = hdr.split()
+        out['signature'] = fields[0][0:5]
+        out['hardblank'] = fields[0][5]
+        out['height']     = int(fields[1])
+        out['baseline']   = int(fields[2])
+        out['max_length'] = int(fields[3])
+        out['old_layout'] = int(fields[4])
+        out['comment_lines'] = int(fields[5])
+        if len(fields) > 6:
+            out['print_direction'] = int(fields[6])
+            out['full_layout']     = int(fields[7])
+            out['codetag_count']   = int(fields[8])
+
+        return out
+        
     def __call__(self,s):
         """Shortcut to render()."""
         return self.render(s)
     
-    @trace
     def __getitem__(self,key):
         """Access a BigLetter from the character representation."""
         try:
             return self.letters[ord(key)]
         except IndexError as e:
             if self.raise_missing:
-                raise KeyError("%s is not present in font")
+                raise KeyError("%s is not present in font" % key)
             else:
                 return self.letters[ord('?')] # fixme, return an empty Bigletter?
 
-    @trace
     def render(self,s):
         """Return string rendered in the font, suitable for printing."""
         return functools.reduce(operator.add, [self[c] for c in s])
